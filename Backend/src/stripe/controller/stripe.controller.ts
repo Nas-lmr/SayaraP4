c25ee1257baea7559d4b6a5993c7142379b7b7be
Sharmake Awil <sharmarke.awil@hotmail.com>
2024-09-19 15:58:07 +0200

    stripe setup backend

---
+++ Backend/src/stripe/controller/stripe.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { StripeService } from '../service/stripe.service';
+import { PaymentRequestBody } from '../PaymentRequestBody';
+import { Response } from 'express'; // Importer Response depuis express
+
+@Controller('payments')
+export class StripeController {
+  constructor(private paymentService: StripeService) {}
+
+  @Post()
+  createPayments(
+    @Res() response: Response, 
+    @Body() paymentRequestBody: PaymentRequestBody,
+  ) {
+    this.paymentService
+      .createPayment(paymentRequestBody)
+      .then((res) => {
+        response.status(HttpStatus.CREATED).json(res); 
+      })
+      .catch((err) => {
+        response.status(HttpStatus.BAD_REQUEST).json(err); 
+      });
+  }
+}
