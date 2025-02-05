6d3320c602eedb5727f004722864cf1a0955d0fa
Sharmake Awil <sharmarke.awil@hotmail.com>
2025-02-05 20:18:45 +0100

    test les test

--- Backend/src/auth/controller/auth.controller.ts
+++ Backend/src/auth/controller/auth.controller.ts
@@ -1,43 +1,4 @@
-// import { Body, Controller, Post } from "@nestjs/common";
-// import { LoginUserDto } from "../dto/auth.dto";
-// import { AuthService } from "../service/auth.service";
-// @Controller()
-// export class AuthController {
-//   constructor(private authService: AuthService) {}
 
-//   @Post("login")
-//   async login(@Body() loginUser: LoginUserDto) {
-//     const user = await this.authService.validateUser(
-//       loginUser.email,
-//       loginUser.password
-//     );
-
-//     return this.authService.login(user);
-//   }
-// }
-
-// import { Body, Controller, Post, Res, HttpException, HttpStatus } from '@nestjs/common';
-// import { Response } from 'express';
-// import { LoginUserDto } from '../dto/auth.dto';  
-// import { AuthService } from '../service/auth.service';
-
-// @Controller() 
-// export class AuthController {
-//   constructor(private authService: AuthService) {}
-
-//   @Post('login')
-//   async login(@Body() loginUser: LoginUserDto, @Res() res: Response) {
-//     // Valider l'utilisateur avec le service
-//     const user = await this.authService.validateUser(loginUser.email, loginUser.password);
-
-//     if (!user) {
-//       throw new HttpException('Email ou mot de passe incorrect', HttpStatus.BAD_REQUEST);
-//     }
-
-//     // Si l'utilisateur est validé, appeler la méthode login pour générer le token et l'envoyer dans le cookie
-//     return this.authService.login(user, res);
-//   }
-// }
 import { Body, Controller, Post, Res, HttpException, HttpStatus, Get, Req } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { LoginUserDto } from '../dto/auth.dto';
