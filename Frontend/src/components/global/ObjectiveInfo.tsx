6b6536cf10503621d138d8c24c0d2091ea2091e1
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2025-02-12 10:29:48 +0100

    test front

--- Frontend/src/components/global/ObjectiveInfo.tsx
+++ Frontend/src/components/global/ObjectiveInfo.tsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import euroIcon from "/src/assets/icons/fi-br-euro.svg";
-import thumbsUpIcon from "/src/assets/icons/fi-br-thumbs-up.svg";
 import leafIcon from "/src/assets/icons/fi-br-leaf.svg";
+import thumbsUpIcon from "/src/assets/icons/fi-br-thumbs-up.svg";
 
 export default function ObjectiveInfo() {
   return (
@@ -86,9 +86,9 @@ export default function ObjectiveInfo() {
               textJustify: "auto",
             }}
           >
-            Réduisez vos frais de transport en partageant vos trajets avec
-            d'autres passagers. Moins de dépenses pour plus de confort, c'est un
-            choix malin pour votre portefeuille.
+            Réduisez vos frais de gfxhxhtrehertransport en partageant vos
+            trajets avec d'autres passagers. Moins de dépenses pour plus de
+            confort, c'est un choix malin pour votre portefeuille.
           </Typography>
         </Box>
         <Box
