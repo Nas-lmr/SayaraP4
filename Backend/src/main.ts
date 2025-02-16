efc41889620ef125e5e718ed9dea7a8d192a6260
Sharmake Awil <sharmarke.awil@hotmail.com>
2025-02-16 13:43:22 +0100

    correction front et ci /cd

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
-  app.setGlobalPrefix("api");
   
   app.enableCors({
     origin: process.env.CLIENT_URL,
